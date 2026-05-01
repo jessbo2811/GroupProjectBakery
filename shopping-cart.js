2c52eea890f0aa853c67c3825580dad301ed7605
karyc <peachy120@gmail.com>
2026-05-01 19:01:33 +0100

    working on shopping-cart.js

--- shopping-cart.js
+++ shopping-cart.js
@@ -1,7 +1,38 @@
 window.addEventListener('load', function() {
 
-    sendButton.addEventListener('click', function (e) {
+    const addProductBtn = document.querySelector('.link-btn');
+
+    addProductBtn.addEventListener("click", function(e) {
+
         e.preventDefault();
 
+
+    })
+
+})
+
+function addProductToCart(product) {
+
+    const productName;
+    const productPrice;
+    const shoppingCart = [];
+
+    if (product.className == "name") {
+        productName = product.innerText;
     }
-});
+
+    if (product.className == "price") {
+        productPrice = product.innerText;
+    }
+
+    const product = {name : productName,
+                     price : productPrice
+    };
+
+    const stringProduct = JSON.stringify(product);
+
+    if (!sessionStorage.getItem('cart')) {
+
+    }
+
+}
